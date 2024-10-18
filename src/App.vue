6fe1cccdf2bbea4a8f54f675ec3a80b408c8ad58
Bruno <b-d.dupuis@laposte.net>
2024-10-18 22:51:42 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 import Details from './components/Details.vue'
-import productInfo from './assets/data.json'
+import data from './assets/data.json'
+
+import { reactive, ref } from 'vue'
+
+const productInfo = reactive(data)
+const selectedVariant = ref(data.variants[0])
 </script>
 
 <template>
@@ -12,10 +17,18 @@ import productInfo from './assets/data.json'
       <!-- wrapper -->
       <div class="container">
         <!-- 1ère colonne -->
-        <div><!-- future image --></div>
+        <div>
+          <img
+            :src="selectedVariant.image.url"
+            :alt="selectedVariant.image.alt"
+          />
+        </div>
         <!-- 2ème colonne -->
 
-        <Details :productInfo="productInfo" />
+        <Details
+          :productInfo="productInfo"
+          :selectedVariant="selectedVariant"
+        />
         <!-- le composant Details remplace l'ancienne structure qui a été transféré dans Details -->
       </div>
     </main>
@@ -40,6 +53,13 @@ import productInfo from './assets/data.json'
 }
 .container > div:last-child {
   padding: 25px 0 25px 100px;
+}
+.container > div > img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  padding: 25px;
+  /* border: 1px solid red; */
 }
 /* la suite du CSS a été tranféré dans le composant Details */
 </style>
--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,11 +1,16 @@
 <script setup>
-const props = defineProps({ productInfo: { type: Object, Required: true } })
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  productInfo: { type: Object, Required: true },
+  selectedVariant: { type: Object, Required: true },
+})
 </script>
 <template>
   <div>
     <h2>{{ productInfo.brand }}</h2>
     <h1>{{ productInfo.name }}</h1>
     <p>{{ productInfo.price }} € <span>TVA incluse</span></p>
+
     <div class="rate">
       <font-awesome-icon :icon="['fas', 'star']" size="lg" />
       <font-awesome-icon :icon="['fas', 'star']" size="lg" />
@@ -14,10 +19,35 @@ const props = defineProps({ productInfo: { type: Object, Required: true } })
       <font-awesome-icon :icon="['fas', 'star-half-alt']" size="lg" />
       <span>{{ productInfo.rate }}</span>
     </div>
-
-    <p class="advise">
-      Nous vous recommandons de choisir une taille au-dessus de celle habituelle.
+    <!-- COULEURS -->
+    <p>
+      Couleur : <span class="selectedColor">{{ selectedVariant.color }}</span>
     </p>
+    <!-- PETITES IMAGES -->
+    <div class="img-bloc">
+      <img
+        v-for="variant in productInfo.variants"
+        :src="variant.image.url"
+        :alt="variant.image.alt"
+        :key="variant.id"
+        :class="{ selectedImg: selectedVariant.id === variant.id }"
+      />
+    </div>
+    <!-- ADVISE -->
+    <p class="advise">
+      Nous vous recommandons de choisir une taille au-dessus de celle
+      habituelle.
+    </p>
+    <!-- SIZE BLOC -->
+    <div class="sizes-bloc">
+      <div
+        v-for="(quantity, size) in selectedVariant.sizes"
+        :key="size"
+        :class="{ outOfStock: quantity === 0 }"
+      >
+        {{ size }}
+      </div>
+    </div>
 
     <div class="cart-bloc">
       <button>Ajouter au panier</button>
@@ -49,7 +79,45 @@ h1 + p span {
 .rate svg {
   margin-right: 3px;
 }
+/* COULEURS */
+.selectedColor {
+  font-weight: bold;
+}
+/* PETITES IMAGES */
+.img-bloc {
+  margin: 10px 0;
+  display: flex;
+  gap: 10px;
+}
+.img-bloc > img {
+  width: 60px;
+  height: 70px;
+  object-fit: cover;
+}
+.selectedImg {
+  border: 2px solid black;
+}
+/* SIZE BLOC */
+.sizes-bloc {
+  display: flex;
+  gap: 10px;
+  margin-bottom: 10px;
+}
 
+.sizes-bloc > div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black;
+  width: 40px;
+  height: 40px;
+  padding-top: 2px;
+  cursor: pointer;
+}
+.sizes-bloc .outOfStock {
+  opacity: 0.5;
+  cursor: default;
+}
 /* ADVISE */
 .advise {
   background-color: var(--light-grey);
