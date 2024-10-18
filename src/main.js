d1384238563b43957e33583058abc3b8efb0fabe
Bruno <b-d.dupuis@laposte.net>
2024-10-18 14:48:53 +0200

    third commit

--- src/main.js
+++ src/main.js
@@ -7,11 +7,15 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* Icône de type 'solid' */
-import { faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+import {
+  faShoppingBag,
+  faStar,
+  faStarHalfAlt,
+} from '@fortawesome/free-solid-svg-icons'
 
 /* Icône de type 'regular' */
 import { faUser, faHeart } from '@fortawesome/free-regular-svg-icons'
 
-library.add(faUser, faHeart, faShoppingBag)
+library.add(faUser, faHeart, faShoppingBag, faStar, faStarHalfAlt)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
